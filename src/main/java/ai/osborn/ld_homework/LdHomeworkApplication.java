0973e90b6131f71c5f36848b02bb52ef26df027f
Jon Osborn <josborn@belltracy.com>
2024-08-06 08:07:06 -0400

    Add spring boot backend with gradle build support

---
+++ src/main/java/ai/osborn/ld_homework/LdHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ai.osborn.ld_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/osborn/ld_homework/LdHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.osborn.ld_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
